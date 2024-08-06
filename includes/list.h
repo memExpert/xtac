36b9973f200e83b1f4741e901e8883f53bda9c32
Maksim Hanchar <pottermax2000@gmail.com>
2024-08-06 21:16:52 +0300

    ADD start of work with API

--- includes/list.h
+++ includes/list.h
@@ -15,4 +15,28 @@ extern LIST* LIST_init(void* data, size_t data_sz);
 extern bool LIST_addf(LIST **first, void const *data, size_t data_sz);
 extern bool LIST_addl(LIST **const first, void const *data, size_t data_sz);
 
+typedef struct linked_list_item
+{
+    struct linked_list_item* next;
+    void*   data;
+} LL_item;
+
+typedef struct linked_list_base {
+    LL_item* first;
+    size_t list_len;
+    size_t data_size;
+} LL_base;
+
+extern LL_base LL_list_init (size_t data_size);
+extern bool LL_list_free (LL_base* to_free);
+extern bool LL_pushf (LL_base *list_base, void const *data);
+extern bool LL_popf (LL_base *list_base, void *data);
+/*
+extern bool LL_pushb(LL_base list_base, void const *data, size_t data_size);
+extern bool LL_popb(LL_base const list_base, void *data, size_t data_size);
+extern bool LL_get_data(LL_item* const item, void const *data);
+extern bool LL_set_data(LL_item* const item, void const *data, size_t data_size);
+extern bool LL_rem_item(LL_base list_base, LL_item* item);
+extern bool LL_add_item(LL_base list_base, LL_item* item);
+*/
 #endif
--- list/list.c
+++ list/list.c
@@ -76,3 +76,140 @@ bool LIST_addl(LIST** const first, const void *data, size_t data_sz) {
     }
     return new_last ? true : false;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+LL_base LL_list_init (size_t data_size) {
+    LL_base list;
+    list.data_size = data_size;
+    list.first = NULL;
+    list.list_len = 0;
+    return list;
+}
+
+LL_item* LL_item_init (void) {
+    LL_item* new_list_item = (LL_item*) malloc(sizeof(LL_item));
+    if(new_list_item) {
+        new_list_item->data = NULL;
+        new_list_item->next = NULL;
+    }
+    return new_list_item; 
+}
+
+void LL_item_free(LL_item* list_item) {
+    if(list_item){
+        free(list_item->data);
+        free(list_item);
+    }
+}
+
+
+static bool LL_inc_len(LL_base* list) {
+    bool result = false;
+    if(list->list_len < SIZE_MAX){
+        list->list_len++;
+        result = true;
+    }
+    return result;
+}
+
+static bool LL_dec_len(LL_base* list) {
+    bool result = false;
+    if(list->list_len > 0) {
+        list->list_len--;
+        result = true;
+    }
+    return result;
+}
+
+
+
+bool LL_list_free (LL_base* list) {
+    bool isFree = false;
+    LL_item* tmp = list->first;
+    if(list) {
+        while (tmp)
+        {
+            list->first = list->first->next;
+            LL_item_free(tmp);
+            tmp = list->first;
+        }
+        list->data_size = 0;
+        list->list_len = 0;
+        isFree = true;
+    } else {
+        isFree = false;
+    }
+    return isFree;
+}
+
+
+bool LL_pushf (LL_base* list_base, const void *data) {
+
+    if(!(list_base && data && LL_inc_len(list_base))) return false;
+    LL_item* new_first = LL_item_init();
+
+    if(!new_first) {
+        LL_dec_len(list_base);
+        return false;
+    }
+    
+    new_first->data = malloc(list_base->data_size);
+    if(!new_first->data) {
+        LL_dec_len(list_base);
+        free(new_first);
+        return false;
+    }
+
+    memmove(new_first->data, data, list_base->data_size);
+    new_first->next = list_base->first;
+    list_base->first = new_first;
+    
+    return true;
+}
+
+bool LL_popf (LL_base* list_base, void *data) {
+    LL_item* tmp = NULL;
+    if(!(list_base && data))  return false;
+    if(!LL_dec_len(list_base)) return false;
+    tmp = list_base->first;
+    if(tmp) {
+        memmove(data, tmp->data, list_base->data_size);
+        list_base->first = tmp->next;
+        LL_item_free(tmp);
+    } else {
+        LL_inc_len(list_base);
+        return false;
+    }
+    return true;
+}
+
+#define COUNT_OF(_x) (sizeof(_x) / sizeof(*_x))
+
+int main(void) {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int result = 0;
+    LL_base list = LL_list_init(sizeof(int));
+
+    for(size_t i = 0; i < COUNT_OF(arr); i++){
+        LL_pushf(&list, (void*)(arr + i));
+        printf("%"PRId64"\n", list.list_len);
+    }
+    while(LL_popf(&list, (void*)&result)) {
+        printf("%d ", result);
+    }
+    printf("\n");
+    return 0;
+}
