a0298fb677c9e4b31c5918c05761e2ee34419ed4
Maksim Hanchar <pottermax2000@gmail.com>
2024-08-06 22:31:41 +0300

    FIX some code maked pretty

--- includes/list.h
+++ includes/list.h
@@ -3,39 +3,37 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-typedef struct list
-{
-    void*  data;
-    size_t data_sz;
-    struct list*   next;
-} LIST;
-
-
-extern LIST* LIST_init(void* data, size_t data_sz);
-extern bool LIST_addf(LIST **first, void const *data, size_t data_sz);
-extern bool LIST_addl(LIST **const first, void const *data, size_t data_sz);
-
 typedef struct linked_list_item
-{
+{   
     struct linked_list_item* next;
     void*   data;
 } LL_item;
 
 typedef struct linked_list_base {
-    LL_item* first;
-    size_t list_len;
-    size_t data_size;
+    LL_item* first;                 /* ptr to first list item */
+    LL_item* last;                  /* ptr to last list item */
+    size_t len;                     /* list length */
+    size_t ds;                      /* data size */
 } LL_base;
-
-extern LL_base LL_list_init (size_t data_size);
+/**
+ * @brief 
+ * 
+ * @param data_size 
+ * @return LL_base 
+ */
+extern LL_base* LL_list_init (size_t data_size);
+extern size_t LL_list_len(const LL_base* list);
 extern bool LL_list_free (LL_base* to_free);
 extern bool LL_pushf (LL_base *list_base, void const *data);
+extern bool LL_pushb (LL_base *list_base, void const *data);
 extern bool LL_popf (LL_base *list_base, void *data);
+extern bool LL_popb (LL_base *list_base, void *data);
+
 /*
-extern bool LL_pushb(LL_base list_base, void const *data, size_t data_size);
-extern bool LL_popb(LL_base const list_base, void *data, size_t data_size);
+
 extern bool LL_get_data(LL_item* const item, void const *data);
-extern bool LL_set_data(LL_item* const item, void const *data, size_t data_size);
+extern bool LL_set_data(LL_item* const item, void const *data);
+extern LL_item LL_get_item(LL_base *list_base, size_t item_n);
 extern bool LL_rem_item(LL_base list_base, LL_item* item);
 extern bool LL_add_item(LL_base list_base, LL_item* item);
 */
--- list/list.c
+++ list/list.c
@@ -1,3 +1,15 @@
+/**
+ * @file list.c
+ * @author Hanchar Maksim (pottermax2000@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
 #include "../includes/list.h"
 #include <inttypes.h>
 #include <malloc.h>
@@ -14,88 +26,56 @@ LIST_update_data, LIST_delitem
     из поля data структуры элемента списка к даблу и переведение запись в dst.
 */
 
-
-
-
-static LIST* LIST_item_init(void) {
-    LIST* new_list_item = (LIST*) malloc(sizeof(LIST));
-    if(new_list_item) {
-        new_list_item->data = NULL;
-        new_list_item->next = NULL;
-        new_list_item->data_sz = 0;
+/*******************************************************************************
+ *                      STATIC FUNCTIONS DEFINITIONS                           *
+ ******************************************************************************/
+static void LL_item_free(LL_item* item) {
+    if(item){
+        free(item->data);
+        free(item);
     }
-    return new_list_item; 
 }
 
-bool LIST_item_set_data(LIST* const item, const void *data, size_t data_sz) {
-    if(item && data){
-        memmove(item->data, data, data_sz);
-        item->data_sz = data_sz;
+
+static bool LL_inc_len(LL_base* list) {
+    bool result = false;
+    if(LL_list_len(list) < SIZE_MAX){
+        list->len++;
+        result = true;
     }
-    return item && data ? true : false;
+    return result;
 }
 
-static LIST* LIST_item_create(const void *data, size_t data_sz) {
-    LIST *item = LIST_item_init();
-
-    if (item){
-        item->data = malloc(data_sz);
-        if(item->data){
-            LIST_item_set_data(item, data, data_sz);
-        }
-        item->next = NULL;
+static bool LL_dec_len(LL_base* list) {
+    bool result = false;
+    if(LL_list_len(list) > 0) {
+        list->len--;
+        result = true;
     }
-    return item;
+    return result;
 }
 
-LIST* LIST_init(void* data, size_t data_sz) {
-    return LIST_item_create(data, data_sz);
-}
 
-bool LIST_addf(LIST** first, const void *data, size_t data_sz) {
-    LIST* new_first = NULL;
-    if(!first || !(*first)) return false;
-    new_first = LIST_item_create(data, data_sz);
-    if(new_first) {
-        new_first->next = *first;
-        *first = new_first;
+/*******************************************************************************
+ *                      GLOBAL FUNCTIONS DEFINITIONS                           *
+ ******************************************************************************/
+
+size_t LL_list_len(const LL_base* list) {
+    if(list) {
+        return list->len;
+    } else {
+        return 0;
     }
-    return new_first ? true : false;
 }
 
-bool LIST_addl(LIST** const first, const void *data, size_t data_sz) {
-    LIST* current = *first;
-    LIST* new_last = NULL;
-    if(!first) return false;
-    new_last = LIST_item_create(data, data_sz);
-    if(new_last) {
-        while(current->next){
-            current = current->next;
-        }
-        current->next = new_last;
+LL_base* LL_list_init (size_t data_size) {
+    LL_base *list = (LL_base*) malloc(sizeof(LL_base));
+    if(list) {
+        list->ds = data_size;
+        list->first = NULL;
+        list->last  = NULL;
+        list->len = 0;
     }
-    return new_last ? true : false;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-LL_base LL_list_init (size_t data_size) {
-    LL_base list;
-    list.data_size = data_size;
-    list.first = NULL;
-    list.list_len = 0;
     return list;
 }
 
@@ -108,46 +88,16 @@ LL_item* LL_item_init (void) {
     return new_list_item; 
 }
 
-void LL_item_free(LL_item* list_item) {
-    if(list_item){
-        free(list_item->data);
-        free(list_item);
-    }
-}
-
-
-static bool LL_inc_len(LL_base* list) {
-    bool result = false;
-    if(list->list_len < SIZE_MAX){
-        list->list_len++;
-        result = true;
-    }
-    return result;
-}
-
-static bool LL_dec_len(LL_base* list) {
-    bool result = false;
-    if(list->list_len > 0) {
-        list->list_len--;
-        result = true;
-    }
-    return result;
-}
-
 
 
 bool LL_list_free (LL_base* list) {
     bool isFree = false;
-    LL_item* tmp = list->first;
+    size_t garbage = 0;
     if(list) {
-        while (tmp)
-        {
-            list->first = list->first->next;
-            LL_item_free(tmp);
-            tmp = list->first;
-        }
-        list->data_size = 0;
-        list->list_len = 0;
+        while (LL_popf(list, &garbage))
+            ;
+        list->ds = 0;
+        list->len = 0;
         isFree = true;
     } else {
         isFree = false;
@@ -158,7 +108,9 @@ bool LL_list_free (LL_base* list) {
 
 bool LL_pushf (LL_base* list_base, const void *data) {
 
-    if(!(list_base && data && LL_inc_len(list_base))) return false;
+    if(!(list_base && data && LL_inc_len(list_base))) {
+        return false;
+    }
     LL_item* new_first = LL_item_init();
 
     if(!new_first) {
@@ -166,14 +118,16 @@ bool LL_pushf (LL_base* list_base, const void *data) {
         return false;
     }
     
-    new_first->data = malloc(list_base->data_size);
+    new_first->data = malloc(list_base->ds);
     if(!new_first->data) {
         LL_dec_len(list_base);
         LL_item_free(new_first);
         return false;
     }
-
-    memmove(new_first->data, data, list_base->data_size);
+    if(LL_list_len(list_base) == 1) {
+        list_base->last = list_base->first;
+    }
+    memmove(new_first->data, data, list_base->ds);
     new_first->next = list_base->first;
     list_base->first = new_first;
     
@@ -182,11 +136,10 @@ bool LL_pushf (LL_base* list_base, const void *data) {
 
 bool LL_popf (LL_base* list_base, void *data) {
     LL_item* tmp = NULL;
-    if(!(list_base && data))  return false;
-    if(!LL_dec_len(list_base)) return false;
+    if(!list_base && data && !LL_dec_len(list_base))  return false;
     tmp = list_base->first;
     if(tmp) {
-        memmove(data, tmp->data, list_base->data_size);
+        memmove(data, tmp->data, list_base->ds);
         list_base->first = tmp->next;
         LL_item_free(tmp);
     } else {
@@ -196,18 +149,28 @@ bool LL_popf (LL_base* list_base, void *data) {
     return true;
 }
 
+bool LL_pushb (LL_base* list_base, void const *data) {
+
+}
+
+bool LL_popb (LL_base* const list_base, void *data) {
+
+}
+
+
+
 #define COUNT_OF(_x) (sizeof(_x) / sizeof(*_x))
 
 int main(void) {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int result = 0;
-    LL_base list = LL_list_init(sizeof(int));
-
+    LL_base* list = LL_list_init(sizeof(int));
+    if(!list) return 1;
     for(size_t i = 0; i < COUNT_OF(arr); i++){
-        LL_pushf(&list, (void*)(arr + i));
-        printf("%"PRId64"\n", list.list_len);
+        LL_pushf(list, (void*)(arr + i));
+        printf("%"PRId64"\n", LL_list_len(list));
     }
-    while(LL_popf(&list, (void*)&result)) {
+    while(LL_popf(list, (void*)&result)) {
         printf("%d ", result);
     }
     printf("\n");
